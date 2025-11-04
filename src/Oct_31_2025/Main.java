8b62a09853d4fef44514d88d15e4e8858e53e069
Kenz <kenz@yahoo.com>
2025-11-04 16:21:29 +0530

    4-NOV-2025 4:21PM IST

--- src/Oct_31_2025/Main.java
+++ src/Oct_31_2025/Main.java
@@ -4,7 +4,7 @@ package Oct_31_2025;
 
 public class Main {
     public static void main(String[] args) {
-        String str="Hello and  welcome!";
+        String str="Hello and  welcome!!!";
         System.out.println(str);
         String[] strArr=str.split("\\s+");
         System.out.println(String.join(" ",strArr));
@@ -12,9 +12,7 @@ public class Main {
         String s1="aA";
         String s2="aAA";
         //s1.length();
-
         System.out.println(s1.compareTo(s2));
-
     }
 
 
